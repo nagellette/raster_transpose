7f7331948b9b64e15ca2ef6329847bdc336ca1c6
nagellette-ws <necipenesgengec@gmail.com>
2019-04-07 13:30:14 +0300

    Reduced the code with wrapping up unnecessary lines.

--- raster_transpose.py
+++ raster_transpose.py
@@ -9,30 +9,28 @@ original_raster_geo_transform = original_raster.GetGeoTransform()
 raster_x_size = original_raster.RasterXSize
 raster_y_size = original_raster.RasterYSize
 
+## get raster band count
+raster_band_count = original_raster.RasterCount
+
 ## create empty raster with 4 bands to produce final raster
-output_raster = gdal.GetDriverByName('GTiff').Create('./data/output_trajectory_new.tif', raster_y_size, raster_x_size, 4,
+output_raster = gdal.GetDriverByName('GTiff').Create('./data/output_trajectory_new.tif', raster_y_size, raster_x_size, raster_band_count,
                                                      gdal.GDT_Float32)
 output_raster.SetGeoTransform(original_raster_geo_transform)
 srs = osr.SpatialReference()
 srs.ImportFromEPSG(32650)
 output_raster.SetProjection(srs.ExportToWkt())  # Exports the coordinate system to the file
 
-## read the raster bands
-band1_np = np.array(original_raster.GetRasterBand(1).ReadAsArray())
-band2_np = np.array(original_raster.GetRasterBand(2).ReadAsArray())
-band3_np = np.array(original_raster.GetRasterBand(3).ReadAsArray())
-band4_np = np.array(original_raster.GetRasterBand(4).ReadAsArray())
 
-## transpose the bands
-band1_np = np.transpose(band1_np)
-band2_np = np.transpose(band2_np)
-band3_np = np.transpose(band3_np)
-band4_np = np.transpose(band4_np)
+for i in range(0, raster_band_count):
+    ## read the raster bands
+    band_np = np.array(original_raster.GetRasterBand(i+1).ReadAsArray())
 
-## pass transposed np arrays as bands
-output_raster.GetRasterBand(1).WriteArray(band1_np)
-output_raster.GetRasterBand(2).WriteArray(band2_np)
-output_raster.GetRasterBand(3).WriteArray(band3_np)
-output_raster.GetRasterBand(4).WriteArray(band4_np)
+    ## transpose the bands
+    band_np = np.transpose(band_np)
 
+    ## pass transposed np arrays as bands
+    output_raster.GetRasterBand(i+1).WriteArray(band_np)
+
+## close the rasters
+original_raster = None
 output_raster = None
